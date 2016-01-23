43d4b398c442fdf6a9963297b639ec3fa03a1e8c
cafeagles <cafeagles@gmail.com>
2016-01-22 23:16:05 -0600

    Initial Commit

---
+++ AngularCodeProjects/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularCodeProjects.MvcApplication" Language="C#" %>
---
+++ AngularCodeProjects/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularCodeProjects.Startup))]
+namespace AngularCodeProjects
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
